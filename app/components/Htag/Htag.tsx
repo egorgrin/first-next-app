31c9ec7061de7c590391f22782a5aaa89e57e044
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-11-15 01:22:07 +0500

    init

---
+++ app/components/Htag/Htag.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react';
+import { HtagProps } from '@/app/components/Htag/Htag.props';
+import s from './Htag.module.css'
+
+export const Htag = ({ tag, children }: HtagProps): ReactElement => {
+  const tagMap: Record<HtagProps['tag'], ReactElement> = {
+    'h1': <h1>{children}</h1>,
+    'h2': <h2>{children}</h2>,
+    'h3': <h3>{children}</h3>,
+  };
+
+  return tagMap[tag] || <></>;
+
+};
